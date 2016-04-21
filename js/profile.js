ce79da1cf6a22d2f4fc2e326a7f9bf8db7eb3481
A6terabytenta <ntatvr@gmail.com>
2016-04-22 00:17:27 +0700

    upload

--- js/profile.js
+++ js/profile.js
@@ -3,6 +3,8 @@ $(function() {
 	profileAddCss();
 
 	profileHoverListener();
+
+	profileOnClick();
 });
 
 function profileHoverListener() {
@@ -20,4 +22,28 @@ function profileAddCss() {
 
 	$('#profile-name-edit').find('.edit-field-icon').hide();
 	$('#profile-career-current-edit').find('.edit-field-icon').hide();
+
+	$('.endorse-plus').show();
+	$('.endorse-plus').css('display', 'inline-flex');
+	$('.endorsing').find('p').hide();
+}
+
+function profileOnClick() {
+
+	/* Begin Endorse */
+
+	/* End Endorse */
+}
+
+function generaterClickEndorse(idParent, classCurrent, classTarget){
+	$('#' + idParent).find('.' + classTarget).css('display', 'inline-flex');
+	$('#' + idParent).find('.' + classCurrent).hide();
+}
+
+function generaterMouseOver(idParent) {
+	$('#' + idParent).find('p').show();
+}
+
+function generaterMouseOut(idParent) {
+	$('#' + idParent).find('p').hide();
 }
