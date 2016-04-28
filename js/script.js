438e08bb605bd284fde8a493752223cd7d6200ad
LocVT <nluit.tanloc@gmail.com>
2016-04-29 01:04:08 +0700

    Post Job

--- js/script.js
+++ js/script.js
@@ -6,5 +6,12 @@ $(function() {
 	$(".menu-item-header").click(function() {
 		$(this).next().toggle();
 	});
+
+	$('select').material_select();
+
+	$("#job_invalidate").pickadate({
+		selectMonths: true,
+		selectYears: 15
+	});
 });
 
